0f0dd9bb9b951bc9d2744555aec9bb5046da0d86
Hoang Tien Son <hoangson02082000@gmail.com>
2021-10-15 10:08:44 +0700

    upload

--- accset/main.js
+++ accset/main.js
@@ -171,3 +171,22 @@ function hienthihang() {
     }
 
 }
+
+function search() {
+    var search, filter, table, tr, td, i, txtValue;
+    search = document.getElementById("search");
+    filter = search.value.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
